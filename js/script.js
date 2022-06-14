9b8e20b491d51db6c1d86bfed5693a6d2bd73926
Basilio-Brad <basiliohinostroza2003bradneve@gmail.com>
2022-06-14 14:27:42 -0500

    commit

--- js/script.js
+++ js/script.js
@@ -2,13 +2,23 @@ const yesBtn = document.querySelector('#yesBtn');
 const pop = document.querySelector('#pop');
 const heart = document.querySelector('#heart');
 
+const back = document.querySelector('#back');
+
 yesBtn.addEventListener('click', function(){
     
     pop.style.setProperty('visibility','visible');
     pop.style.setProperty('display','block');
 
     heart.style.setProperty('visibility','hidden');
+
+    back.style.setProperty('background-image', 'url(img/back.jpg)');
+
+    noBtn.style.setProperty('visibility','hidden');
+
     
+    var yes = "si";
+    guardar_valores(yes);
+
 })
 
 const noBtn = document.querySelector('#noBtn');
@@ -37,5 +47,19 @@ acceptBtn.addEventListener('click', function(){
     pop.style.setProperty('display','none');
 
     heart.style.setProperty('visibility','visible');
+    noBtn.style.setProperty('visibility','visible');
+    back.style.setProperty('background-image', 'none');
     
 })
+
+function guardar_valores (object) {
+  
+    localStorage.setItem("acepta", object);
+  }
+
+function recuperar_datos () {
+    var peticion = localStorage.getItem("acepta");
+    alert(peticion);
+  }
+
+ 
